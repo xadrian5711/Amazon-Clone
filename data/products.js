eb040fb4b1bdbc6fa2890ba33a6f6c0b369aaf86
xadrian5711 <xadrian5711@icloud.com>
2026-02-11 09:36:59 -0700

    project started, file system updated, product info added, tailwind linked, nav bar started.

---
+++ data/products.js
@@ -0,0 +1,57 @@
+const products = [
+    {
+        id: 1,
+        name: "basketball",
+        description: "Round rubber ball for shooting hoops",
+        image: "../download.jpg",
+        price: 15.99,
+    },
+    {
+        id: 2,
+        name: "Nike Air Max",
+        description: "Best shoes for basketball on the planet",
+        image: "../download-1.jpg",
+        price: 79.99,
+    },
+    {
+        id: 3,
+        name: "Basketball Shorts Nike",
+        description: "For amazing air breeze and comfort while on the court.",
+        image: "download-2.jpg",
+        price: 39.99,
+    },
+    {
+        id: 4,
+        name: "Nike Shirt",
+        description: "Basketball never stops", 
+        image: "download-3.jpg",
+        price: 20.99,
+    },
+    {
+        id: 5,
+        name: "Nike Socks",
+        description: "Elite Nike socks.",
+        image: "../shopping.webp",
+        price: 15.99,
+    },
+    {
+        id: 6,
+        name: "Gatorade",
+        description: "Power fuel for Ballers",
+        image: "../shopping-1.webp",
+        price: 12.99,
+    },{
+        id: 7,
+        name: "Jordan Shoes",
+        description: "Best Shoe invented by baller MJ",
+        image: "../download-4.jpg",
+        price: 109.99,
+    },{
+        id: 8,
+        name: "HeyDude",
+        description: "comfy slip on shoes",
+        image: "../download-5.jpg",
+        price: 59.99,
+    }
+
+]
